c8a86cbf9740b37ef0f659c2d89cb28c53fd1166
Ali Atilgan <a.atilgan1720@gmail.com>
2021-09-11 21:44:47 -0500

    first commit

---
+++ src/test/java/com/techproed/pages/PositiveTestPage.java
@@ -0,0 +1,42 @@
+package com.techproed.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.techproed.utilities.Driver;
+
+public class PositiveTestPage {
+    public PositiveTestPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath="//*[@id='navLogon']")
+    public WebElement login;
+
+    @FindBy(id="UserName")
+    public WebElement username;
+
+    @FindBy(id="Password")
+    public WebElement password;
+
+    @FindBy(id="btnSubmit")
+    public WebElement submitbutton;
+
+    @FindBy(xpath="//ul[@class='sub-menu']")
+    public WebElement listOfUsers;
+
+    @FindBy(id="divMessageResult")
+    public WebElement errorMessage;
+
+    @FindBy(linkText="Hotel Management")
+    public WebElement hotelManagement;
+
+    @FindBy(partialLinkText = "Hotel Rooms")
+    public WebElement hotelRooms;
+
+    @FindBy(xpath = "(//span)[1]")
+    public WebElement userID;
+
+
+}
